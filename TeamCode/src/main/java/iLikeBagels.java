dada8a77144d1ca576b51b5a6723b8ba747ede33
Sid_Ramnathsing <sid.ramnathsing@gmail.com>
2026-06-22 13:35:28 +0200

    sasd

---
+++ TeamCode/src/main/java/iLikeBagels.java
@@ -0,0 +1,22 @@
+
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Basic",group = "Linear OpMode")
+@Disabled
+public class iLikeBagels extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor = 
+
+
+}
+
+public void runOpMode(){
+
+
+}
